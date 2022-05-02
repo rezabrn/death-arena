58a0e4f39207a90ca7479b8d2a5a0e57feb44547
unknown <reza.salehi16161616@gmail.com>
2022-05-02 23:42:05 +0430

    complete Main class

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -3,12 +3,16 @@ package ir.ac.kntu;
 import ir.ac.kntu.logic.ConsoleGraphicsEngine;
 import ir.ac.kntu.logic.Director;
 import ir.ac.kntu.logic.GraphicsEngine;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        GraphicsEngine engine = new ConsoleGraphicsEngine(); 
-        Director director = new Director(engine); 
-        //TODO: Start your game from here
+        System.out.println("Please enter the number of a group: ");
+        int counter = (new Scanner(System.in)).nextInt();
+        Director director = new Director(counter);
+
+        director.startGameLoop();
+
     }
-    
+
 }
